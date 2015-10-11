9aeae2095f9ce610b94caff4cb85c1860e26e9e9
Jeremiah Lee <jeremiah.lee@gmail.com>
2015-10-11 14:08:07 -0700

    Update paths.

--- bash_profile_includes/Darwin/paths.sh
+++ bash_profile_includes/Darwin/paths.sh
@@ -1,1 +1,1 @@
-PATH=$HOME/bin:"$(brew --prefix coreutils)/libexec/gnubin":/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:~/.bash_profile_includes/bin:/sbin:/bin:$GOPATH/bin:$PATH:$HOME/.rvm/bin
+PATH=$HOME/bin:"$(brew --prefix coreutils)/libexec/gnubin":/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:~/.bash_profile_includes/bin:/sbin:/bin:$GOPATH/bin:$PATH:$HOME/.rvm/bin:/usr/local/texlive/2012/bin/universal-darwin/
