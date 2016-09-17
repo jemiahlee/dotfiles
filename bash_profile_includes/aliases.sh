c2ff88a8eb4645fccec72a7c328157a9e3e72c61
Jeremiah Lee <jeremiah.lee@gmail.com>
2016-09-16 23:20:08 -0700

    Update aliases a bit.

--- bash_profile_includes/aliases.sh
+++ bash_profile_includes/aliases.sh
@@ -1,5 +1,5 @@
 function current_git_branch_bare {
-	echo `git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+    echo `git branch --no-color 2> /dev/null | perl -n -e 'next unless /^\s*\*\s*(?:.\d+.\s*)?(.*)/; print "$1"; exit;'`
 }
 
 alias ag='ag --ignore \*.json'
@@ -7,6 +7,7 @@ alias ag='ag --ignore \*.json'
 alias g=git
 alias gpr="git pull --rebase"
 alias gmu="git fetch upstream && git merge --ff-only upstream/master"
+alias gru="git fetch upstream && git rebase upstream/master"
 alias gitprune="git remote prune origin"
 alias gst="git about && echo && git status"
 alias gfa="git fetch upstream && git fetch"
@@ -29,3 +30,4 @@ alias ..........="cd ../../../../../../../../.."
 
 alias ls="ls -p --color"
 alias vi='vi -u /usr/share/vim/vimrc'
+alias act="source venv/bin/activate"
