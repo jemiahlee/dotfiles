e62ddbe79eeb6d33ba86c9c8d6dde03b1d369338
Jeremiah Lee <jeremiah.lee@gmail.com>
2017-04-09 21:41:43 -0700

    Change git branch name color based on whether the branch is dirty.

--- bash_profile_includes/ps1.sh
+++ bash_profile_includes/ps1.sh
@@ -1,7 +1,22 @@
 function current_git_branch {
-    echo `git branch --no-color 2> /dev/null | perl -n -e 'next unless /^\s*\*\s*(?:.\d+.\s*)?(.*)/; print "[$1]"; exit;'`
+    branchname=`git branch --no-color 2> /dev/null | perl -n -e 'next unless /^\s*\*\s*(?:.\d+.\s*)?(.*)/; print "[$1]"; exit;'`
+
+    if [[ ! -z $branchname ]]; then
+      dirty=`git status --short`
+
+      RED='\033[0;31m'
+      WHITE='\033[0;37m'
+      NC='\033[0m'
+
+      if [[ -z $dirty ]]; then
+        echo -e "${WHITE}${branchname} "
+      else
+        echo -e "${RED}${branchname} "
+      fi
+    fi
+
 }
 
 export CLICOLOR=1
-export PS1="\[\033[36m\]\h:\w \[\033[33m\]\$(current_git_branch)\[\033[00m\]$\[\033[00m\] "
+export PS1="\[\033[36m\]\h:\w \$(current_git_branch)\[\033[36m\]$\[\033[0m\] "
 export SUDO_PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[0;31m\]\$ \[\e[0m\]'
