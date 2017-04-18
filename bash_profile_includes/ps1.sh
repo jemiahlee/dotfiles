fe5e9238cd198d1fd794c049e4a8838e6a8f8f50
Jeremiah Lee <jeremiah.lee@gmail.com>
2017-04-18 06:47:16 -0700

    Fix the prompt back to one color. Wrapping issues with the other way.

--- bash_profile_includes/ps1.sh
+++ bash_profile_includes/ps1.sh
@@ -1,32 +1,7 @@
 function current_git_branch {
-    # echo -e `print_branch`
-    BRANCH=`print_branch`
-    local exit=$?
-
-    if [[ -z "$BRANCH" ]]; then
-      exit
-    fi
-
-    YELLOW='\033[0;33m'
-    WHITE='\033[1;37m'
-    RED='\033[0;31m'
-    GREEN='\033[0;32m'
-    DEFAULT='\033[0m'
-
-    if [[ $exit -eq 0 ]]; then
-      echo -n -e "$GREEN"
-    elif [[ $exit -eq 1 ]]; then
-      echo -n -e "$RED"
-    elif [[ $exit -eq 2 ]]; then
-      echo -n -e $YELLOW
-    elif [[ $exit -eq 3 ]]; then
-      echo -n -e $WHITE
-    fi
-
-    echo -n $BRANCH
-    echo -e "$DEFAULT"
+    echo `git branch --no-color 2> /dev/null | perl -n -e 'next unless /^\s*\*\s*(?:.\d+.\s*)?(.*)/; print "[$1]"; exit;'`
 }
 
 export CLICOLOR=1
-export PS1="\[\033[0;36m\]\h:\w \$(current_git_branch)\[\033[0;36m\]$\[\033[0m\] "
+export PS1="\[\033[36m\]\h:\w \[\033[33m\]\$(current_git_branch)\[\033[00m\]$\[\033[00m\] "
 export SUDO_PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[0;31m\]\$ \[\e[0m\]'
