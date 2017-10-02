40cfddcf95028ca35a929ff34cd9454a4c6ecf3e
Jeremiah Lee <jeremiah.lee@gmail.com>
2017-10-02 20:45:20 +0000

    Make SSH configuration predictable on virtual hosts.

---
+++ bash_profile_includes/Linux/ssh-agent.sh
@@ -0,0 +1,8 @@
+# Predictable SSH authentication socket location.
+SOCK="/tmp/ssh-agent-$USER-screen"
+if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
+then
+    rm -f /tmp/ssh-agent-$USER-screen
+    ln -sf $SSH_AUTH_SOCK $SOCK
+    export SSH_AUTH_SOCK=$SOCK
+fi
