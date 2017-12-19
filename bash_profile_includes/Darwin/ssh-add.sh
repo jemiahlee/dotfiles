9d11202bdf14bb9c39f0e843c9ec0cc504268d0a
Jeremiah Lee <jeremiah.lee@gmail.com>
2017-12-19 13:45:26 -0800

    Fixing SSH configuration so that I do not have to change the plist config.

--- bash_profile_includes/Darwin/ssh-add.sh
+++ bash_profile_includes/Darwin/ssh-add.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -x -i
 
 SSH_KEY_LIST="ssh-add -l"
+AGENT_FILE="${HOME}/.agent-env"
+SSH_LIST=`ssh-add -l`
 
-if [ "x" == "x`ps -x -u ${USER} | egrep [s]sh-agent | egrep -v egrep`" ] ; then
-	ssh-agent | sed -e "/^echo/d" > ${HOME}/.agent-env
+if [[ ! -f $AGENT_FILE || "${SSH_LIST}" == "Error connecting to agent: No such file or directory" ]]; then
+  echo "Removing $AGENT_FILE"
+  rm $AGENT_FILE
+  echo "Setting up ssh-agent"
+  ssh-agent | sed -e "/^echo/d" > ${HOME}/.agent-env
 fi
-source ${HOME}/.agent-env
 
-for file in /Users/"$USER"/.ssh/*id_rsa
+source $AGENT_FILE
+
+for file in /Users/"$USER"/.ssh/id_rsa
 do
 	present=`$SSH_KEY_LIST | /usr/bin/grep ${file}`
 
