877c8f1b78ac9a938ec61b29765568e362dc4a5f
Jeremiah Lee <jeremiah.lee@gmail.com>
2017-12-21 14:26:04 -0800

    Make the SSH configuration deterministic based on the agent file in my home directory.

--- bash_profile_includes/Darwin/ssh-add.sh
+++ bash_profile_includes/Darwin/ssh-add.sh
@@ -4,9 +4,7 @@ SSH_KEY_LIST="ssh-add -l"
 AGENT_FILE="${HOME}/.agent-env"
 SSH_LIST=`ssh-add -l`
 
-if [[ ! -f $AGENT_FILE || "${SSH_LIST}" == "Error connecting to agent: No such file or directory" ]]; then
-  echo "Removing $AGENT_FILE"
-  rm $AGENT_FILE
+if [[ ! -f $AGENT_FILE ]]; then
   echo "Setting up ssh-agent"
   ssh-agent | sed -e "/^echo/d" > ${HOME}/.agent-env
 fi
