a98837ca1b14589dd6c01cf793d33a9914192769
Jeremiah Lee <jeremiah.lee@gmail.com>
2019-01-18 14:52:52 -0800

    Some more aliases to fetch just origin master.

--- bash_profile_includes/aliases.sh
+++ bash_profile_includes/aliases.sh
@@ -8,7 +8,7 @@ alias g=git
 alias gpr="git pull --rebase"
 alias gmo="git fetch origin && git merge --ff-only origin/master"
 alias gmu="git fetch upstream && git merge --ff-only upstream/master"
-alias gro="git fetch origin && git rebase origin/master"
+alias gro="git fetch origin master && git rebase origin/master"
 alias gru="git fetch upstream && git rebase upstream/master"
 alias gitprune="git remote prune origin"
 alias gst="git about && echo && git status"
@@ -18,7 +18,10 @@ alias fff="git fetch origin && git merge --ff-only origin/\$(current_git_branch_
 alias ffu="git merge --ff-only upstream/\$(current_git_branch_bare)"
 alias fffu="git fetch upstream && git merge --ff-only upstream/\$(current_git_branch_bare)"
 alias fu="git fetch upstream"
+alias fum="git fetch upstream master"
 alias fo="git fetch origin"
+alias fom="git fetch origin master"
+alias ffom="git fetch origin master && git merge --ff-only origin/master"
 
 alias ll="ls -lah"
 
