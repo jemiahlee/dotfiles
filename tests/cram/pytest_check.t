3c7f559e75c36d2f9c43e97d80dc28a18da51b96
Jeremiah Lee <jeremiah.lee@gmail.com>
2019-12-08 21:57:36 -0500

    WIP

---
+++ tests/cram/pytest_check.t
@@ -0,0 +1,2 @@
+  $ $BIN/pytest_check $TESTDIR/pytest_check_input.py
+  $ $BIN/pytest_check $TESTDIR
---
+++ tests/cram/pytest_check_input.py
@@ -0,0 +1,14 @@
+assert_equals(var, var2)
+assert_equals(var, True)
+assert_equals(var, False)
+assertEqual(var, var2)
+assertEquals(var, True)
+assertEquals(var, False)
+assertTrue(var)
+assertFalse(var)
+
+
+assertTrue(var > var2)
+assertTrue(var >= var2)
+assertTrue(var < var2)
+assertTrue(var <= var2)
