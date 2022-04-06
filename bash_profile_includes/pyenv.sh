b0cfdb6d0fd2e1ccdc712c0cff3eb373e84c4315
Jeremiah Lee <jeremiah.lee@gmail.com>
2022-04-06 09:45:24 -0700

    Configuration for pyenv

---
+++ bash_profile_includes/pyenv.sh
@@ -0,0 +1,19 @@
+export PYENV_SHELL=bash
+source '/opt/homebrew/Cellar/pyenv/2.2.5/libexec/../completions/pyenv.bash'
+command pyenv rehash 2>/dev/null
+pyenv() {
+  local command
+  command="${1:-}"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  activate|deactivate|rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")"
+    ;;
+  *)
+    command pyenv "$command" "$@"
+    ;;
+  esac
+}
