8a87e7a2a72b504aa0d35513bd336da1db16e5e8
Jeremiah Lee <jeremiah.lee@gmail.com>
2022-12-19 13:26:03 -0800

    Make SSH config smarter - if the socket no longer exists, start over.

--- bash_profile_includes/Darwin/ssh-add.sh
+++ bash_profile_includes/Darwin/ssh-add.sh
@@ -4,6 +4,14 @@ SSH_KEY_LIST="ssh-add -l"
 AGENT_FILE="${HOME}/.agent-env"
 SSH_LIST=`ssh-add -l`
 
+if [[ -f $AGENT_FILE ]]; then
+  source $AGENT_FILE
+  if [ ! -e "$SSH_AUTH_SOCK" ]; then
+    echo "Agent file out of date, removing..."
+    rm $AGENT_FILE
+  fi
+fi
+
 if [[ ! -f $AGENT_FILE ]]; then
   echo "Setting up ssh-agent"
   ssh-agent | sed -e "/^echo/d" > ${HOME}/.agent-env
