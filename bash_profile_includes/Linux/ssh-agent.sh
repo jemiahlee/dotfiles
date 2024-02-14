cec16a1f48419ab727b7d542fc8371dc5be7677f
Jeremiah Lee <jelee@admin1nuq.prod.easypo.net>
2024-02-14 00:31:31 +0000

    Update SSH socket to be in private directory rather than /tmp

--- bash_profile_includes/Linux/ssh-agent.sh
+++ bash_profile_includes/Linux/ssh-agent.sh
@@ -1,8 +1,14 @@
-# Predictable SSH authentication socket location.
-SOCK="/tmp/ssh-agent-$USER-screen"
+# Predictable SSH socket location
+SOCK_DIR="/dev/shm/.ssh-$USER"
+SOCK="$SOCK_DIR/ssh-agent"
 if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
 then
-    rm -f /tmp/ssh-agent-$USER-screen
+    rm -f $SOCK
+    if [ ! -d "$SOCK_DIR" ]; then
+      mkdir $SOCK_DIR
+      chown $USER:$USER $SOCK_DIR
+      chmod 700 $SOCK_DIR
+    fi
     ln -sf $SSH_AUTH_SOCK $SOCK
     export SSH_AUTH_SOCK=$SOCK
 fi
