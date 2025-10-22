b1678308dcddf0cb9d9d3455c0dcac7d8468ba6d
Jeremiah Lee <jeremiah.lee@gmail.com>
2025-10-22 21:50:42 +0000

    bash config: split the .bash_local into .bash_local_pre and .bash_local_post so that there is some granularity to when code runs vis a vis the rest of the configuration.

--- bash_profile_includes/ps1.sh
+++ bash_profile_includes/ps1.sh
@@ -6,5 +6,5 @@ function current_git_branch {
 }
 
 export CLICOLOR=1
-export PS1="\[\033[36m\]$(hostname) \[\033[35m\](\t) \[\033[32m\]\w \[\033[33m\]\$(current_git_branch)\[\033[00m\]$\[\033[00m\] "
+export PS1="\[\033[36m\]$($HOME/bin/hostname) \[\033[35m\](\t) \[\033[32m\]\w \[\033[33m\]\$(current_git_branch)\[\033[00m\]$\[\033[00m\] "
 export SUDO_PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[0;31m\]\$ \[\e[0m\]'
